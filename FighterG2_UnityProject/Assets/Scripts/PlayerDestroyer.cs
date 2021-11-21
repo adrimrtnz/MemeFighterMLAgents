a837c2d1915a5cede73feb90442e85f0d0ec7fcd
Jorge <jorgebaegar3@gmail.com>
2021-11-21 14:27:08 +0100

    Destroyer
    PlayerDestroyer

--- FighterG2_UnityProject/Assets/Scripts/PlayerDestroyer.cs
+++ FighterG2_UnityProject/Assets/Scripts/PlayerDestroyer.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class PlayerDestroyer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D colision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    
+        if (colision.CompareTag("Destroy"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
